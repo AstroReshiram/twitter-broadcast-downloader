5b42809b2a6d4d473262bd26f6276f1c1ff985b8
AstroReshiram <66257895+AstroReshiram@users.noreply.github.com>
2024-12-14 17:26:05 +0000

    Added a UI quickly. nothing else fancy

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
-# Twitter Broadcast Downloader
-Download Twitter broadcasts/lives. 
+# Twitter Broadcast Downloader ui
 
-## How does it work?
-Input an URL (e.g. https://twitter.com/i/broadcasts/s0M3th1nG).
-The script will then run Selenium to get the ".m3u8 address" which is hosted on `<something>.video.pscp.tv` or something similar.
-The address we are looking for is loaded after the page has loaded in, so the script adds an event listener to listen for all XHR events and try to find the m3u8.
-If the listener finds the data that includes the m3u8 address, it will throw an error to the console, where the Python script is listening for errors.
-If the error includes the m3u8 address it will extract it and download it through FFMPEG.
-You could also copy the address, and play it through VLC, if you just want to view the broadcast "locally".
+This is a fork of https://github.com/offish/twitter-broadcast-downloader except I added a UI to it.
+
+Absolutely nothing else is special about it. Go there and read their ReadMe for better details.
 
 ## Installation
 ```
--- main.py
+++ main.py
@@ -1,37 +1,86 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QWidget, QVBoxLayout, QLabel
+from PySide6.QtCore import Qt
+
 from downloader.utils import *
-
-# import codecs
-
 from selenium import webdriver
+
+import validators
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Twitter Broadcast Downloader")
+
+        # Center the window on the screen
+        screen_size = QApplication.primaryScreen().availableGeometry()
+
+        width = 350
+        height = 50
+        self.setGeometry((screen_size.width() - width) // 2, (screen_size.height() - height) // 2, width, height)
+        
+        widget = QWidget(self)
+        self.setCentralWidget(widget)
+
+        layout = QVBoxLayout(widget)
+
+        # Create a text box
+        self.text_box = QLineEdit(self)
+        self.download_button = QPushButton("Download", self)
+        self.download_button.setFixedWidth(self.width() // 2) 
+        self.download_button.clicked.connect(self.download)
+
+        self.label = QLabel("hiii")
+
+        layout.addWidget(self.text_box)
+        layout.addWidget(self.label, alignment=Qt.AlignCenter)
+        layout.addWidget(self.download_button, alignment=Qt.AlignCenter)
+
+    def download(self):
+        url = self.text_box.text()
+        url = url if "http://" in url or "https://" in url else "https://" + url
+        result = validators.url(url)
+
+        if result and "twitter.com" in url:
+            self.label.setText("Downloading!")
+            self._run_download(url)
+        else:
+            self.label.setText("Not a valid twitter url!!!!!")
+
+    def _run_download(self, url):
+        driver = webdriver.Chrome()
+
+        driver.get(url)
+        driver.execute_script(open("downloader/listener.js", "r").read())
+
+        data = ""
+
+        while not data:
+            for i in driver.get_log("browser"):
+                if (
+                    "message" in i
+                    and "source" in i
+                    and i["source"] == "console-api"
+                    and ".m3u8" in i["message"]
+                ):
+                    data = i["message"]
+
+        driver.quit()
+
+        # file = codecs.open("temp", "w", "utf-8")
+        # file.write(data)
+        # file.close()
+
+        # data = open("temp", "r", encoding="utf-8").read()
+        m3u8_url = extract_m3u8_url(data)
+
+        download(m3u8_url)
+        # remove_file("temp")
+        self.label.setText("Done!")
 
 
 if __name__ == "__main__":
-    url = input("twitter broadcast url: ")
-    driver = webdriver.Chrome()
-    driver.get(url)
-    driver.execute_script(open("downloader/listener.js", "r").read())
-
-    data = ""
-
-    while not data:
-        for i in driver.get_log("browser"):
-            if (
-                "message" in i
-                and "source" in i
-                and i["source"] == "console-api"
-                and ".m3u8" in i["message"]
-            ):
-                data = i["message"]
-
-    driver.quit()
-
-    # file = codecs.open("temp", "w", "utf-8")
-    # file.write(data)
-    # file.close()
-
-    # data = open("temp", "r", encoding="utf-8").read()
-    m3u8_url = extract_m3u8_url(data)
-
-    download(m3u8_url)
-    # remove_file("temp")
-    print("Done!")
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
